c7e0027f8845ca2573b24a30bbf7dfa91720d81c
Natanael <neithanmo@gmail.com>
2022-05-03 17:01:24 -0600

    Add error definitions

---
+++ app/src/handlers/parser_common.rs
@@ -0,0 +1,53 @@
+use nom::error::ErrorKind;
+
+#[repr(u32)]
+#[derive(Copy, Clone, PartialEq)]
+#[cfg_attr(test, derive(Debug))]
+pub enum ParserError {
+    // Generic errors
+    NoData,
+    DisplayIdxOutOfRange,
+    DisplayPageOutOfRange,
+    UnexpectedError,
+    // Required fields
+    // Coin specific
+    InvalidHashMode,
+    InvalidSignature,
+    InvalidPubkeyEncoding,
+    InvalidAddressVersion,
+    UnexpectedType,
+    UnexpectedBufferEnd,
+    UnexpectedNumberItems,
+    UnexpectedField,
+    ValueOutOfRange,
+    InvalidAddress,
+}
+
+impl From<ErrorKind> for ParserError {
+    fn from(err: ErrorKind) -> Self {
+        match err {
+            ErrorKind::Eof => ParserError::UnexpectedBufferEnd,
+            ErrorKind::Permutation => ParserError::UnexpectedType,
+            ErrorKind::TooLarge => ParserError::ValueOutOfRange,
+            _ => ParserError::UnexpectedError,
+        }
+    }
+}
+
+impl<I> nom::error::ParseError<I> for ParserError {
+    fn from_error_kind(_input: I, kind: ErrorKind) -> Self {
+        Self::from(kind)
+    }
+
+    // We don't have enough memory resources to use here an array with the last
+    // N errors to be used as a backtrace, so that, we just propagate here the latest
+    // reported error
+    fn append(_input: I, _kind: ErrorKind, other: Self) -> Self {
+        other
+    }
+}
+impl From<ParserError> for nom::Err<ParserError> {
+    fn from(error: ParserError) -> Self {
+        nom::Err::Error(error)
+    }
+}
