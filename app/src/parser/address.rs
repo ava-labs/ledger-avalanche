d1012abc761d2561a366199436dfc496c474bae0
Natanael <neithanmo@gmail.com>
2022-05-08 08:27:27 -0600

    Implement DispalyableItem for Address

--- app/src/parser/address.rs
+++ app/src/parser/address.rs
@@ -1,9 +1,32 @@
-use nom::{bytes::complete::take, error::ErrorKind, IResult};
+/*******************************************************************************
+*   (c) 2021 Zondax GmbH
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+********************************************************************************/
+use core::{mem::MaybeUninit, ptr::addr_of_mut};
+use nom::{bytes::complete::take, IResult};
 
-use crate::handlers::parser_common::ParserError;
+use crate::{
+    handlers::{handle_ui_message, parser_common::ParserError},
+    parser::DisplayableItem,
+};
 
-pub const ADDRESS_LEN: usize = 20;
+use crate::sys::{bech32, hash::Ripemd160};
+use zemu_sys::ViewError;
 
+pub const ADDRESS_LEN: usize = Ripemd160::DIGEST_LEN;
+
+// ripemd160(sha256(compress(secp256k1.publicKey()))
 #[derive(Clone, Copy, PartialEq)]
 #[cfg_attr(test, derive(Debug))]
 pub struct Address<'b>(&'b [u8; ADDRESS_LEN]);
@@ -14,5 +37,58 @@ impl<'b> Address<'b> {
         let (left, addr) = take(ADDRESS_LEN)(input)?;
         let addr = arrayref::array_ref!(addr, 0, ADDRESS_LEN);
         Ok((left, Self(addr)))
+    }
+
+    #[inline(never)]
+    pub fn from_bytes_into(
+        input: &'b [u8],
+        out: &mut MaybeUninit<Self>,
+    ) -> Result<&'b [u8], nom::Err<ParserError>> {
+        let (rem, addr) = take(ADDRESS_LEN)(input)?;
+        let addr = arrayref::array_ref!(addr, 0, ADDRESS_LEN);
+
+        let out = out.as_mut_ptr();
+
+        //good ptr and no uninit reads
+        unsafe {
+            addr_of_mut!((*out).0).write(addr);
+        }
+
+        Ok(rem)
+    }
+}
+
+impl<'a> DisplayableItem for Address<'a> {
+    fn num_items(&self) -> usize {
+        1
+    }
+
+    #[inline(never)]
+    fn render_item(
+        &self,
+        item_n: u8,
+        title: &mut [u8],
+        message: &mut [u8],
+        page: u8,
+    ) -> Result<u8, ViewError> {
+        use bolos::{pic_str, PIC};
+
+        if item_n != 0 {
+            return Err(ViewError::NoData);
+        }
+
+        const MAX_SIZE: usize = bech32::estimate_size(0, Ripemd160::DIGEST_LEN);
+
+        let mut addr = [0; MAX_SIZE];
+
+        // TODO what hrp do we use?
+        // AFAICT presence of the hrp only alters the error correction at the end
+        // of the encoded address
+        let len = bech32::encode("", &self.0, &mut addr[..]).map_err(|_| ViewError::Unknown)?;
+
+        let title_content = pic_str!(b"Address");
+        title[..title_content.len()].copy_from_slice(title_content);
+
+        handle_ui_message(&addr[..len], message, page)
     }
 }
