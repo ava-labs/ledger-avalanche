5f5309d7130d46a7cef3f4ec17cbd89282e60b78
Natanael <neithanmo@gmail.com>
2022-05-22 13:51:45 -0600

    Add enum to wrapper transferable outputs

--- app/src/parser/outputs.rs
+++ app/src/parser/outputs.rs
@@ -18,7 +18,175 @@ mod nft_mint_output;
 mod nft_transfer_output;
 mod secp_mint_output;
 mod secp_transfer_output;
+use arrayref::array_ref;
 pub use nft_mint_output::NFTMintOutput;
 pub use nft_transfer_output::NFTTransferOutput;
 pub use secp_mint_output::SECPMintOutput;
 pub use secp_transfer_output::SECPTransferOutput;
+
+use core::{mem::MaybeUninit, ptr::addr_of_mut};
+use nom::{bytes::complete::take, number::complete::be_u32};
+use zemu_sys::ViewError;
+
+use crate::handlers::handle_ui_message;
+
+use crate::parser::{error::ParserError, AssetId, DisplayableItem};
+
+// Important: do not change the repr attribute,
+// as this type is use as the tag field
+// for the Output enum which has the same representation
+#[derive(Clone, Copy, PartialEq)]
+#[repr(u8)]
+#[derive(Debug)]
+enum OutputType {
+    SECPTransfer,
+    SECPMint,
+    NFTTransfer,
+    NFTMint,
+}
+
+impl OutputType {
+    fn from_bytes(input: &[u8]) -> Result<(&[u8], Self), nom::Err<ParserError>> {
+        let (rem, variant_type) = be_u32(input)?;
+
+        let v = match variant_type {
+            SECPTransferOutput::TYPE_ID => Self::SECPTransfer,
+            SECPMintOutput::TYPE_ID => Self::SECPMint,
+
+            NFTTransferOutput::TYPE_ID => Self::NFTTransfer,
+            NFTMintOutput::TYPE_ID => Self::NFTMint,
+            _ => return Err(ParserError::InvalidTypeId.into()),
+        };
+
+        Ok((rem, v))
+    }
+}
+
+#[repr(C)]
+struct SECPTransferVariant<'b>(OutputType, SECPTransferOutput<'b>);
+
+#[repr(C)]
+struct SECPMintVariant<'b>(OutputType, SECPMintOutput<'b>);
+
+#[repr(C)]
+struct NFTTransferVariant<'b>(OutputType, NFTTransferOutput<'b>);
+
+#[repr(C)]
+struct NFTMintVariant<'b>(OutputType, NFTMintOutput<'b>);
+
+#[derive(Clone, Copy, PartialEq)]
+// DO not change the representation
+// as it would cause unalignment issues
+// with the OutputType tag
+#[repr(u8)]
+#[cfg_attr(test, derive(Debug))]
+pub enum Output<'b> {
+    SECPTransfer(SECPTransferOutput<'b>),
+    SECPMint(SECPMintOutput<'b>),
+    NFTTransfer(NFTTransferOutput<'b>),
+    NFTMint(NFTMintOutput<'b>),
+}
+
+impl<'b> Output<'b> {
+    #[cfg(test)]
+    fn from_bytes(input: &'b [u8]) -> Result<(&'b [u8], Self), nom::Err<ParserError>> {
+        let mut out = MaybeUninit::uninit();
+        let rem = Self::from_bytes_into(input, &mut out)?;
+        unsafe { Ok((rem, out.assume_init())) }
+    }
+
+    #[inline(never)]
+    pub fn from_bytes_into(
+        input: &'b [u8],
+        out: &mut MaybeUninit<Self>,
+    ) -> Result<&'b [u8], nom::Err<ParserError>> {
+        crate::sys::zemu_log_stack("Output::from_bytes_into\x00");
+
+        let (rem, variant_type) = OutputType::from_bytes(input)?;
+        let rem = match variant_type {
+            OutputType::SECPTransfer => {
+                let out = out.as_mut_ptr() as *mut SECPTransferVariant;
+                //valid pointer
+                let data = unsafe { &mut *addr_of_mut!((*out).1).cast() };
+
+                let rem = SECPTransferOutput::from_bytes_into(rem, data)?;
+
+                //pointer is valid
+                unsafe {
+                    addr_of_mut!((*out).0).write(OutputType::SECPTransfer);
+                }
+
+                rem
+            }
+            OutputType::SECPMint => {
+                let out = out.as_mut_ptr() as *mut SECPMintVariant;
+                //valid pointer
+                let data = unsafe { &mut *addr_of_mut!((*out).1).cast() };
+
+                let rem = SECPMintOutput::from_bytes_into(rem, data)?;
+
+                //pointer is valid
+                unsafe {
+                    addr_of_mut!((*out).0).write(OutputType::SECPMint);
+                }
+
+                rem
+            }
+            OutputType::NFTTransfer => {
+                let out = out.as_mut_ptr() as *mut NFTTransferVariant;
+                //valid pointer
+                let data = unsafe { &mut *addr_of_mut!((*out).1).cast() };
+
+                let rem = NFTTransferOutput::from_bytes_into(rem, data)?;
+
+                //pointer is valid
+                unsafe {
+                    addr_of_mut!((*out).0).write(OutputType::NFTTransfer);
+                }
+                rem
+            }
+            OutputType::NFTMint => {
+                let out = out.as_mut_ptr() as *mut NFTMintVariant;
+                //valid pointer
+                let data = unsafe { &mut *addr_of_mut!((*out).1).cast() };
+
+                let rem = NFTMintOutput::from_bytes_into(rem, data)?;
+
+                //pointer is valid
+                unsafe {
+                    addr_of_mut!((*out).0).write(OutputType::NFTMint);
+                }
+                rem
+            }
+            _ => return Err(ParserError::InvalidTypeId.into()),
+        };
+        Ok(rem)
+    }
+}
+
+impl<'a> DisplayableItem for Output<'a> {
+    fn num_items(&self) -> usize {
+        match self {
+            Self::SECPTransfer(t) => t.num_items(),
+            Self::SECPMint(m) => m.num_items(),
+            Self::NFTTransfer(t) => t.num_items(),
+            Self::NFTMint(m) => m.num_items(),
+        }
+    }
+
+    #[inline(never)]
+    fn render_item(
+        &self,
+        item_n: u8,
+        title: &mut [u8],
+        message: &mut [u8],
+        page: u8,
+    ) -> Result<u8, ViewError> {
+        match self {
+            Self::SECPTransfer(t) => t.render_item(item_n, title, message, page),
+            Self::SECPMint(m) => m.render_item(item_n, title, message, page),
+            Self::NFTTransfer(t) => t.render_item(item_n, title, message, page),
+            Self::NFTMint(m) => m.render_item(item_n, title, message, page),
+        }
+    }
+}
