d7c49161c74005627dad2062393e8180031f379c
Natanael <neithanmo@gmail.com>
2022-06-17 16:35:01 -0600

    feat(tx) Add initial parser for ImportTxs

--- app/src/parser/transactions/import_tx.rs
+++ app/src/parser/transactions/import_tx.rs
@@ -29,14 +29,7 @@ use crate::{
 };
 
 const SOURCE_CHAIN_LEN: usize = BLOCKCHAIN_ID_LEN;
-
-pub fn check_import_tx_types(tx_type: u32) -> bool {
-    // The ExportTx type is the same for avm and pvm
-    // virtual machines. so we need to check if
-    // the passed in id corresponds to one of these 2 possible
-    // transactions.
-    tx_type == PVM_IMPORT_TX || tx_type == AVM_IMPORT_TX
-}
+const IMPORT_DESCRIPTION_LEN: usize = 7;
 
 #[derive(Clone, Copy, PartialEq)]
 #[cfg_attr(test, derive(Debug))]
@@ -56,8 +49,12 @@ impl<'b> FromBytes<'b> for ImportTx<'b> {
         crate::sys::zemu_log_stack("BaseTx::from_bytes_into\x00");
 
         let (rem, type_id) = be_u32(input)?;
-        // double check
-        if !check_import_tx_types(type_id) {
+
+        // The ImportTx type is the same for avm and pvm
+        // virtual machines. so we need to check if
+        // the passed in id corresponds to one of these 2 possible
+        // transactions.
+        if !(type_id == PVM_IMPORT_TX || type_id == AVM_IMPORT_TX) {
             return Err(ParserError::InvalidTransactionType.into());
         }
 
@@ -79,7 +76,7 @@ impl<'b> FromBytes<'b> for ImportTx<'b> {
             _ => return Err(ParserError::InvalidTransactionType.into()),
         }
 
-        // Exporting to the same chain is an error
+        // Importing from the same chain is an error
         if dest_chain_id == base_chain_id {
             return Err(ParserError::InvalidTransactionType.into());
         }
@@ -95,5 +92,201 @@ impl<'b> FromBytes<'b> for ImportTx<'b> {
         }
 
         Ok(rem)
+    }
+}
+
+impl<'b> ImportTx<'b> {
+    fn fee(&self) -> Result<u64, ParserError> {
+        let inputs = self.sum_inputs_amount()?;
+        let outputs = self.sum_base_outputs_amount()?;
+
+        let fee = inputs
+            .checked_sub(outputs)
+            .ok_or(ParserError::OperationOverflows)?;
+        Ok(fee)
+    }
+
+    fn fee_to_fp_str(&self, out_str: &'b mut [u8]) -> Result<&mut [u8], ParserError> {
+        use lexical_core::{write as itoa, Number};
+
+        let fee = self.fee()?;
+
+        // the number plus '0.'
+        if out_str.len() < usize::FORMATTED_SIZE_DECIMAL + 2 {
+            return Err(ParserError::UnexpectedBufferEnd);
+        }
+
+        itoa(fee, out_str);
+        intstr_to_fpstr_inplace(out_str, NANO_AVAX_DECIMAL_DIGITS)
+            .map_err(|_| ParserError::UnexpectedError)
+    }
+
+    fn sum_inputs_amount(&self) -> Result<u64, ParserError> {
+        let base_inputs = self
+            .base_tx
+            .inputs
+            .iter::<TransferableInput>()
+            .map(|input| {
+                if let Ok(input) = input {
+                    return input.amount().ok_or(ParserError::UnexpectedError);
+                }
+                Err(ParserError::UnexpectedError)
+            })
+            .try_fold(0u64, |acc, x| {
+                let x = x?;
+                acc.checked_add(x).ok_or(ParserError::OperationOverflows)
+            })?;
+        let import_inputs = self
+            .inputs
+            .iter::<TransferableInput>()
+            .map(|input| {
+                if let Ok(input) = input {
+                    return input.amount().ok_or(ParserError::UnexpectedError);
+                }
+                Err(ParserError::UnexpectedError)
+            })
+            .try_fold(0u64, |acc, x| {
+                let x = x?;
+                acc.checked_add(x).ok_or(ParserError::OperationOverflows)
+            })?;
+        import_inputs
+            .checked_add(base_inputs)
+            .ok_or(ParserError::OperationOverflows)
+    }
+
+    fn sum_base_outputs_amount(&self) -> Result<u64, ParserError> {
+        self.base_tx
+            .outputs
+            .iter::<TransferableOutput>()
+            .map(|output| {
+                if let Ok(output) = output {
+                    return output.amount().ok_or(ParserError::UnexpectedError);
+                }
+                Err(ParserError::UnexpectedError)
+            })
+            .try_fold(0u64, |acc, x| {
+                let x = x?;
+                acc.checked_add(x).ok_or(ParserError::OperationOverflows)
+            })
+    }
+
+    fn num_input_items(&self) -> usize {
+        self.base_tx
+            .outputs
+            .iter::<TransferableOutput>()
+            .flatten()
+            .map(|output| output.num_items())
+            .sum()
+    }
+
+    // use base.outputs, which contains the amount and address(es)
+    // the founds is being transfered to
+    fn render_imports(
+        &self,
+        item_n: u8,
+        title: &mut [u8],
+        message: &mut [u8],
+        page: u8,
+    ) -> Result<u8, zemu_sys::ViewError> {
+        let mut count = 0usize;
+        let mut obj_item_n = 0;
+        // gets the SECPTranfer output that contains item_n
+        // and its corresponding index
+        let filter = |o: &TransferableOutput| -> bool {
+            let n = o.num_items();
+            for index in 0..n {
+                count += 1;
+                obj_item_n = index;
+                if count == item_n as usize + 1 {
+                    return true;
+                }
+            }
+            false
+        };
+
+        let obj = self
+            .base_tx
+            .outputs
+            .iter::<TransferableOutput>()
+            .flatten()
+            .find(filter)
+            .ok_or(ViewError::NoData)?;
+
+        obj.render_item(obj_item_n as u8, title, message, page)
+    }
+
+    fn render_import_description(
+        &self,
+        title: &mut [u8],
+        message: &mut [u8],
+        page: u8,
+    ) -> Result<u8, zemu_sys::ViewError> {
+        use arrayvec::ArrayString;
+        use bolos::{pic_str, PIC};
+
+        let title_content = pic_str!(b"From ");
+        title[..title_content.len()].copy_from_slice(title_content);
+
+        // render from where this transaction is moving founds to
+        let mut export_str: ArrayString<IMPORT_DESCRIPTION_LEN> = ArrayString::new();
+        let from_alias = chain_alias_lookup(self.source_chain).map_err(|_| ViewError::Unknown)?;
+
+        // can we use format! or a better alternative
+        // would we have problems with PIC because of
+        // the literal strings bellow, the assumpion is that
+        // it is copied into.
+        export_str.push_str(from_alias);
+        export_str.push_str(" Chain");
+
+        handle_ui_message(export_str.as_bytes(), message, page)
+    }
+}
+
+impl<'b> DisplayableItem for ImportTx<'b> {
+    fn num_items(&self) -> usize {
+        // only support SECP256k1 outputs
+        // and to keep compatibility with the legacy app,
+        // we show only 4 items for each output
+        // tx info, amount, address and fee which is the sum of all inputs minus all outputs
+        // and the chain description
+        1 + self.num_input_items() + 1 + 1
+    }
+
+    fn render_item(
+        &self,
+        item_n: u8,
+        title: &mut [u8],
+        message: &mut [u8],
+        page: u8,
+    ) -> Result<u8, zemu_sys::ViewError> {
+        use bolos::{pic_str, PIC};
+        use lexical_core::Number;
+
+        if item_n == 0 {
+            // render export title and network info
+            //return self.render_export_description(title, message, page);
+            let title_content = pic_str!(b"ImportTx");
+            title[..title_content.len()].copy_from_slice(title_content);
+            let value_content = pic_str!(b"Sending");
+            return handle_ui_message(&value_content[..], message, page);
+        }
+
+        let inputs_num_items = self.num_input_items() as u8;
+        let new_item_n = item_n - 1;
+
+        match new_item_n {
+            x @ 0.. if x < inputs_num_items as u8 => self.render_imports(x, title, message, page),
+            x if x == inputs_num_items => self.render_import_description(title, message, page),
+            x if x == (inputs_num_items + 1) => {
+                let title_content = pic_str!(b"Fee");
+                title[..title_content.len()].copy_from_slice(title_content);
+                let mut buffer = [0; usize::FORMATTED_SIZE + 2];
+                let fee_str = self
+                    .fee_to_fp_str(&mut buffer[..])
+                    .map_err(|_| ViewError::Unknown)?;
+                handle_ui_message(fee_str, message, page)
+            }
+            _ => Err(ViewError::NoData),
+        }
     }
 }
