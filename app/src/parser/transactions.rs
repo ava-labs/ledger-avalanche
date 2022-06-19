7a4018700d6922c21e9b21a52317b874b80ac295
Natanael <neithanmo@gmail.com>
2022-06-19 09:59:39 -0600

    initial parsing of import_tx

--- app/src/parser/transactions.rs
+++ app/src/parser/transactions.rs
@@ -16,5 +16,124 @@
 
 mod base_tx;
 mod export_tx;
+mod import_tx;
+
+use core::convert::TryFrom;
+
+use crate::parser::constants::*;
+use crate::parser::ParserError;
+
 pub use base_tx::{BaseTx, BLOCKCHAIN_ID_LEN};
 pub use export_tx::ExportTx;
+pub use import_tx::ImportTx;
+
+#[derive(Clone, Copy, PartialEq)]
+#[repr(u8)]
+#[cfg_attr(test, derive(Debug))]
+pub enum PvmTransactionTypes {
+    AddValidator,
+    AddSubnetValidator,
+    AddDelegator,
+    CreateChain,
+    CreateSubnet,
+    Export,
+    Import,
+}
+
+impl TryFrom<u32> for PvmTransactionTypes {
+    type Error = ParserError;
+
+    fn try_from(value: u32) -> Result<Self, Self::Error> {
+        let res = match value {
+            PVM_CREATE_SUBNET => Self::CreateSubnet,
+            PVM_EXPORT_TX => Self::Export,
+            PVM_IMPORT_TX => Self::Import,
+            PVM_ADD_VALIDATOR => Self::AddValidator,
+            PVM_ADD_SUBNET_VALIDATOR => Self::AddSubnetValidator,
+            PVM_ADD_DELEGATOR => Self::AddDelegator,
+            PVM_CREATE_CHAIN => Self::CreateChain,
+            _ => return Err(ParserError::InvalidNetworkId),
+        };
+
+        Ok(res)
+    }
+}
+
+#[derive(Clone, Copy, PartialEq)]
+#[repr(u8)]
+#[cfg_attr(test, derive(Debug))]
+pub enum AvmTransactionTypes {
+    CreateAsset,
+    Operation,
+    Export,
+    Import,
+}
+
+impl TryFrom<u32> for AvmTransactionTypes {
+    type Error = ParserError;
+
+    fn try_from(value: u32) -> Result<Self, Self::Error> {
+        let res = match value {
+            AVM_CREATE_ASSET_TX => Self::CreateAsset,
+            AVM_OPERATION_TX => Self::Operation,
+            AVM_IMPORT_TX => Self::Import,
+            AVM_EXPORT_TX => Self::Export,
+            _ => return Err(ParserError::InvalidNetworkId),
+        };
+
+        Ok(res)
+    }
+}
+
+#[derive(Clone, Copy, PartialEq)]
+#[repr(u8)]
+#[cfg_attr(test, derive(Debug))]
+pub enum EvmTransactionTypes {
+    Import,
+    Export,
+}
+
+impl TryFrom<u32> for EvmTransactionTypes {
+    type Error = ParserError;
+
+    fn try_from(value: u32) -> Result<Self, Self::Error> {
+        let res = match value {
+            EVM_IMPORT_TX => Self::Import,
+            EVM_EXPORT_TX => Self::Export,
+            _ => return Err(ParserError::InvalidNetworkId),
+        };
+
+        Ok(res)
+    }
+}
+
+#[derive(Clone, Copy, PartialEq)]
+#[repr(u8)]
+#[cfg_attr(test, derive(Debug))]
+pub enum TransactionType {
+    Avm(AvmTransactionTypes),
+    Evm(EvmTransactionTypes),
+    Pvm(PvmTransactionTypes),
+}
+
+impl TransactionType {
+    pub fn is_pvm_tx(&self) -> bool {
+        matches!(self, Self::Pvm(..))
+    }
+
+    pub fn is_avm_tx(&self) -> bool {
+        matches!(self, Self::Avm(..))
+    }
+
+    pub fn is_evm_tx(&self) -> bool {
+        matches!(self, Self::Evm(..))
+    }
+}
+
+impl TryFrom<(&[u8; BLOCKCHAIN_ID_LEN], u32)> for TransactionType {
+    type Error = ParserError;
+
+    fn try_from(value: (&[u8; BLOCKCHAIN_ID_LEN], u32)) -> Result<Self, Self::Error> {
+        todo!()
+    }
+}
