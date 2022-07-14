6e9839d20ae7ad2d33088785bb2498d41f0f3c62
Natanael <neithanmo@gmail.com>
2022-07-14 12:33:17 -0600

    feat(outputs:generic): Make TransferableOutputs generic over the inner output, and add a new variant to the Outputs enum, that is used in p-chain

--- app/src/parser/outputs.rs
+++ app/src/parser/outputs.rs
@@ -13,6 +13,11 @@
 *  See the License for the specific language governing permissions and
 *  limitations under the License.
 ********************************************************************************/
+use core::ops::Deref;
+
+use crate::parser::{AssetId, DisplayableItem, FromBytes, ParserError};
+use crate::sys::ViewError;
+use core::{mem::MaybeUninit, ptr::addr_of_mut};
 
 mod nft_mint_output;
 mod nft_transfer_output;
@@ -25,23 +30,42 @@ pub use secp_mint_output::SECPMintOutput;
 pub use secp_output_owners::SECPOutputOwners;
 pub use secp_transfer_output::SECPTransferOutput;
 
-use core::{mem::MaybeUninit, ptr::addr_of_mut};
-use nom::number::complete::be_u32;
-use zemu_sys::ViewError;
-
-use crate::parser::{error::ParserError, AssetId, DisplayableItem, FromBytes};
-
 #[derive(Clone, Copy, PartialEq)]
 #[repr(C)]
 #[cfg_attr(test, derive(Debug))]
-pub struct TransferableOutput<'b> {
+pub struct TransferableOutput<'b, O>
+where
+    O: FromBytes<'b> + DisplayableItem + Deref<Target = Output<'b>> + 'b,
+{
     asset_id: AssetId<'b>,
-    output: Output<'b>,
+    pub output: O,
 }
 
-impl<'b> TransferableOutput<'b> {
-    pub fn amount(&self) -> Option<u64> {
-        self.output.amount()
+impl<'b, O> Deref for TransferableOutput<'b, O>
+where
+    O: FromBytes<'b> + DisplayableItem + Deref<Target = Output<'b>> + 'b,
+{
+    type Target = O::Target;
+
+    fn deref(&self) -> &Self::Target {
+        &*self.output
+    }
+}
+
+impl<'b, O> TransferableOutput<'b, O>
+where
+    O: FromBytes<'b> + DisplayableItem + Deref<Target = Output<'b>> + 'b,
+{
+    pub fn get_amount(&self) -> Option<u64> {
+        (&*self.output).amount()
+    }
+
+    pub fn assert_id(&self) -> &AssetId<'b> {
+        &self.asset_id
+    }
+
+    pub fn output(&self) -> &Output<'b> {
+        &*self.output
     }
 
     pub fn output(&self) -> &Output<'_> {
@@ -49,7 +73,10 @@ impl<'b> TransferableOutput<'b> {
     }
 }
 
-impl<'b> FromBytes<'b> for TransferableOutput<'b> {
+impl<'b, O> FromBytes<'b> for TransferableOutput<'b, O>
+where
+    O: FromBytes<'b> + DisplayableItem + Deref<Target = Output<'b>> + 'b,
+{
     #[inline(never)]
     fn from_bytes_into(
         input: &'b [u8],
@@ -57,17 +84,23 @@ impl<'b> FromBytes<'b> for TransferableOutput<'b> {
     ) -> Result<&'b [u8], nom::Err<ParserError>> {
         crate::sys::zemu_log_stack("TransferableOutput::from_bytes_into\x00");
 
-        let output = out.as_mut_ptr() as *mut TransferableOutput;
+        let output = out.as_mut_ptr() as *mut TransferableOutput<O>;
+        //valid pointer
         let asset = unsafe { &mut *addr_of_mut!((*output).asset_id).cast() };
         let rem = AssetId::from_bytes_into(input, asset)?;
 
         //valid pointer
         let data = unsafe { &mut *addr_of_mut!((*output).output).cast() };
-        Output::from_bytes_into(rem, data)
+        let rem = O::from_bytes_into(rem, data)?;
+
+        Ok(rem)
     }
 }
 
-impl<'b> DisplayableItem for TransferableOutput<'b> {
+impl<'b, O> DisplayableItem for TransferableOutput<'b, O>
+where
+    O: FromBytes<'b> + DisplayableItem + Deref<Target = Output<'b>> + 'b,
+{
     fn num_items(&self) -> usize {
         // the asset_id is not part of the summary we need from objects of this type,
         // but could give to higher level objects information to display such information.
@@ -88,30 +121,14 @@ impl<'b> DisplayableItem for TransferableOutput<'b> {
 // Important: do not change the repr attribute,
 // as this type is use as the tag field
 // for the Output enum which has the same representation
-#[derive(Clone, Copy, PartialEq)]
+#[derive(Clone, Copy, PartialEq, Debug)]
 #[repr(u8)]
-enum OutputType {
+pub enum OutputType {
     SECPTransfer,
     SECPMint,
     NFTTransfer,
     NFTMint,
-}
-
-impl OutputType {
-    fn from_bytes(input: &[u8]) -> Result<(&[u8], Self), nom::Err<ParserError>> {
-        let (rem, variant_type) = be_u32(input)?;
-
-        let v = match variant_type {
-            SECPTransferOutput::TYPE_ID => Self::SECPTransfer,
-            SECPMintOutput::TYPE_ID => Self::SECPMint,
-
-            NFTTransferOutput::TYPE_ID => Self::NFTTransfer,
-            NFTMintOutput::TYPE_ID => Self::NFTMint,
-            _ => return Err(ParserError::InvalidTypeId.into()),
-        };
-
-        Ok((rem, v))
-    }
+    SECPOwners,
 }
 
 #[repr(C)]
@@ -126,6 +143,9 @@ struct NFTTransferVariant<'b>(OutputType, NFTTransferOutput<'b>);
 #[repr(C)]
 struct NFTMintVariant<'b>(OutputType, NFTMintOutput<'b>);
 
+#[repr(C)]
+struct SECPOwnersVariant<'b>(OutputType, SECPOutputOwners<'b>);
+
 #[derive(Clone, Copy, PartialEq)]
 // DO not change the representation
 // as it would cause unalignment issues
@@ -137,27 +157,33 @@ pub enum Output<'b> {
     SECPMint(SECPMintOutput<'b>),
     NFTTransfer(NFTTransferOutput<'b>),
     NFTMint(NFTMintOutput<'b>),
+    SECPOwners(SECPOutputOwners<'b>),
 }
 
 impl<'b> Output<'b> {
+    pub fn secp_transfer(&self) -> Option<&SECPTransferOutput> {
+        if let Self::SECPTransfer(ref secp) = self {
+            Some(secp)
+        } else {
+            None
+        }
+    }
     pub fn amount(&self) -> Option<u64> {
         match self {
             Self::SECPTransfer(secp) => Some(secp.amount),
             _ => None,
         }
     }
-}
 
-impl<'b> FromBytes<'b> for Output<'b> {
     #[inline(never)]
-    fn from_bytes_into(
+    pub fn from_bytes(
         input: &'b [u8],
+        variant: OutputType,
         out: &mut MaybeUninit<Self>,
     ) -> Result<&'b [u8], nom::Err<ParserError>> {
-        crate::sys::zemu_log_stack("Output::from_bytes_into\x00");
+        crate::sys::zemu_log_stack("Output::from_bytes\x00");
 
-        let (_, variant_type) = OutputType::from_bytes(input)?;
-        let rem = match variant_type {
+        let rem = match variant {
             OutputType::SECPTransfer => {
                 let out = out.as_mut_ptr() as *mut SECPTransferVariant;
                 //valid pointer
@@ -212,6 +238,20 @@ impl<'b> FromBytes<'b> for Output<'b> {
                 }
                 rem
             }
+            OutputType::SECPOwners => {
+                let out = out.as_mut_ptr() as *mut SECPOwnersVariant;
+                //valid pointer
+                let data = unsafe { &mut *addr_of_mut!((*out).1).cast() };
+
+                let rem = SECPOutputOwners::from_bytes_into(input, data)?;
+
+                //pointer is valid
+                unsafe {
+                    addr_of_mut!((*out).0).write(OutputType::SECPOwners);
+                }
+
+                rem
+            }
         };
         Ok(rem)
     }
@@ -224,6 +264,7 @@ impl<'a> DisplayableItem for Output<'a> {
             Self::SECPMint(m) => m.num_items(),
             Self::NFTTransfer(t) => t.num_items(),
             Self::NFTMint(m) => m.num_items(),
+            Self::SECPOwners(o) => o.num_items(),
         }
     }
 
@@ -240,6 +281,7 @@ impl<'a> DisplayableItem for Output<'a> {
             Self::SECPMint(m) => m.render_item(item_n, title, message, page),
             Self::NFTTransfer(t) => t.render_item(item_n, title, message, page),
             Self::NFTMint(m) => m.render_item(item_n, title, message, page),
+            Self::SECPOwners(o) => o.render_item(item_n, title, message, page),
         }
     }
 }
@@ -247,6 +289,7 @@ impl<'a> DisplayableItem for Output<'a> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::parser::{AvmOutput, PvmOutput};
 
     const DATA: &[u8] = &[
         1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
@@ -255,8 +298,15 @@ mod tests {
     ];
 
     #[test]
-    fn parse_transferable_output() {
-        let t = TransferableOutput::from_bytes(DATA).unwrap().1;
-        assert!(matches!(t.output, Output::NFTMint(..)));
+    fn parse_transferable_output_pvm_output() {
+        let res = TransferableOutput::<PvmOutput>::from_bytes(DATA);
+        // should faild as output for pvm only support secp_transfer and owners
+        assert!(res.is_err());
+    }
+
+    #[test]
+    fn parse_transferable_output_avm_output() {
+        let t = TransferableOutput::<AvmOutput>::from_bytes(DATA).unwrap().1;
+        assert!(matches!(t.output.0, Output::NFTMint(..)));
     }
 }
