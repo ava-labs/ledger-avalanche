9787efb40153078bd285cab8fd4281d34d4b6ead
Natanael <neithanmo@gmail.com>
2022-07-17 08:29:25 -0600

    feat(generic_output): Add a wrapper around the Output type

--- app/src/parser/coreth/outputs.rs
+++ app/src/parser/coreth/outputs.rs
@@ -14,15 +14,84 @@
 *  limitations under the License.
 ********************************************************************************/
 
-use core::ptr::addr_of_mut;
+use core::ops::Deref;
 
-use nom::number::complete::be_u64;
+use core::{mem::MaybeUninit, ptr::addr_of_mut};
+use nom::number::complete::{be_u32, be_u64};
 use zemu_sys::ViewError;
 
 use crate::{
     handlers::handle_ui_message,
-    parser::{Address, AssetId, DisplayableItem, FromBytes},
+    parser::{
+        error::ParserError, Address, AssetId, DisplayableItem, FromBytes, Output, OutputType,
+        SECPTransferOutput,
+    },
 };
+
+// Wrapper for the Output type
+#[derive(Clone, Copy, PartialEq)]
+#[repr(C)]
+#[cfg_attr(test, derive(Debug))]
+pub struct EOutput<'b>(pub Output<'b>);
+
+impl<'b> EOutput<'b> {
+    pub fn amount(&self) -> Option<u64> {
+        self.0.amount()
+    }
+}
+
+impl<'b> Deref for EOutput<'b> {
+    type Target = Output<'b>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<'b> FromBytes<'b> for EOutput<'b> {
+    #[inline(never)]
+    fn from_bytes_into(
+        input: &'b [u8],
+        out: &mut MaybeUninit<Self>,
+    ) -> Result<&'b [u8], nom::Err<ParserError>> {
+        let variant_type = Self::parse_output_type(input)?;
+        let out = out.as_mut_ptr();
+        //valid pointer
+        let data = unsafe { &mut *addr_of_mut!((*out).0).cast() };
+        let rem = Output::from_bytes(input, variant_type, data)?;
+        Ok(rem)
+    }
+}
+
+impl<'b> EOutput<'b> {
+    fn parse_output_type(input: &[u8]) -> Result<OutputType, nom::Err<ParserError>> {
+        let (_, variant_type) = be_u32(input)?;
+
+        // Coreth only supports the SECPTransferOutput variant
+        let v = match variant_type {
+            SECPTransferOutput::TYPE_ID => OutputType::SECPTransfer,
+            _ => return Err(ParserError::InvalidTypeId.into()),
+        };
+
+        Ok(v)
+    }
+}
+
+impl<'b> DisplayableItem for EOutput<'b> {
+    fn num_items(&self) -> usize {
+        self.0.num_items()
+    }
+
+    fn render_item(
+        &self,
+        item_n: u8,
+        title: &mut [u8],
+        message: &mut [u8],
+        page: u8,
+    ) -> Result<u8, ViewError> {
+        self.0.render_item(item_n as _, title, message, page)
+    }
+}
 
 #[derive(Clone, Copy, PartialEq)]
 #[repr(C)]
