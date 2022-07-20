3e4a95ec374bc9fa5c9d23bc42aac43220c61516
Natanael <neithanmo@gmail.com>
2022-07-20 08:44:08 -0600

    feat(p-chain): Implement parsing/ui for AddSubnetValidator transactions

--- app/src/parser/transactions/pvm.rs
+++ app/src/parser/transactions/pvm.rs
@@ -1,9 +1,11 @@
+mod add_subnet_validator;
 mod add_validator;
 mod create_chain_tx;
 mod create_subnet_tx;
 mod export_tx;
 mod import_tx;
 
+pub use add_subnet_validator::*;
 pub use add_validator::*;
 pub use create_chain_tx::*;
 pub use create_subnet_tx::*;
