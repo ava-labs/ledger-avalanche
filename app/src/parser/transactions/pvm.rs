214128834c0e77bfa22605507e27947559759f6c
Natanael <neithanmo@gmail.com>
2022-07-20 20:52:37 -0600

    feat(p-chain): Initial support for add_delegator transaction

--- app/src/parser/transactions/pvm.rs
+++ app/src/parser/transactions/pvm.rs
@@ -1,3 +1,4 @@
+mod add_delegator;
 mod add_subnet_validator;
 mod add_validator;
 mod create_chain_tx;
---
+++ app/src/parser/transactions/pvm/add_delegator.rs
@@ -0,0 +1,436 @@
+/*******************************************************************************
+*   (c) 2021 Zondax GmbH
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+********************************************************************************/
+use bolos::{pic_str, PIC};
+use core::{mem::MaybeUninit, ptr::addr_of_mut};
+use nom::{bytes::complete::tag, number::complete::be_u32};
+use zemu_sys::ViewError;
+
+use super::AVAX_TO;
+use crate::{
+    handlers::handle_ui_message,
+    parser::{
+        intstr_to_fpstr_inplace, nano_avax_to_fp_str, timestamp_to_str_date, Address, BaseTxFields,
+        DisplayableItem, FromBytes, Header, ObjectList, ParserError, PvmOutput, SECPOutputOwners,
+        TransferableOutput, Validator, DELEGATION_FEE_DIGITS, MAX_ADDRESS_ENCODED_LEN,
+        PVM_ADD_DELEGATOR,
+    },
+};
+
+#[derive(Clone, Copy, PartialEq)]
+#[repr(C)]
+#[cfg_attr(test, derive(Debug))]
+pub struct AddDelegatorTx<'b> {
+    pub tx_header: Header<'b>,
+    pub base_tx: BaseTxFields<'b, PvmOutput<'b>>,
+    pub validator: Validator<'b>,
+    pub stake: ObjectList<'b, TransferableOutput<'b, PvmOutput<'b>>>,
+    pub rewards_owner: SECPOutputOwners<'b>,
+}
+
+impl<'b> FromBytes<'b> for AddDelegatorTx<'b> {
+    #[inline(never)]
+    fn from_bytes_into(
+        input: &'b [u8],
+        out: &mut MaybeUninit<Self>,
+    ) -> Result<&'b [u8], nom::Err<ParserError>> {
+        crate::sys::zemu_log_stack("AddDelegatorTx::from_bytes_into\x00");
+
+        let (rem, _) = tag(PVM_ADD_DELEGATOR.to_be_bytes())(input)?;
+
+        let out = out.as_mut_ptr();
+
+        // tx header
+        let tx_header = unsafe { &mut *addr_of_mut!((*out).tx_header).cast() };
+        let rem = Header::from_bytes_into(rem, tx_header)?;
+
+        // base_tx
+        let base_tx = unsafe { &mut *addr_of_mut!((*out).base_tx).cast() };
+        let rem = BaseTxFields::<PvmOutput>::from_bytes_into(rem, base_tx)?;
+
+        // validator
+        let validator = unsafe { &mut *addr_of_mut!((*out).validator).cast() };
+        let rem = Validator::from_bytes_into(rem, validator)?;
+
+        // stake
+        let stake = unsafe { &mut *addr_of_mut!((*out).stake).cast() };
+        let rem = ObjectList::<TransferableOutput<PvmOutput>>::new_into(rem, stake)?;
+
+        // valid pointers read as memory was initialized
+        let staked_list = unsafe { &*stake.as_ptr() };
+
+        let validator_stake = unsafe { (*validator.as_ptr()).weight };
+
+        // get locked outputs amount to check for invariant
+        let stake = Self::sum_stake_outputs_amount(staked_list)?;
+
+        // Check for invariant, the locked utxos must be equals to validators' stake
+        if validator_stake != stake {
+            return Err(ParserError::InvalidStakingAmount.into());
+        }
+
+        // rewards_owner
+        let rewards_owner = unsafe { &mut *addr_of_mut!((*out).rewards_owner).cast() };
+        let rem = SECPOutputOwners::from_bytes_into(rem, rewards_owner)?;
+
+        Ok(rem)
+    }
+}
+
+impl<'b> DisplayableItem for AddDelegatorTx<'b> {
+    fn num_items(&self) -> usize {
+        // tx_info, base_tx items, validator_items(4),
+        // fee, fee_delegation, rewards_to and stake items
+        1 + self.base_tx.base_outputs_num_items()
+            + self.validator.num_items()
+            + self.rewards_owner.addresses.len()
+            + self.num_stake_items()
+            + 1
+    }
+
+    fn render_item(
+        &self,
+        item_n: u8,
+        title: &mut [u8],
+        message: &mut [u8],
+        page: u8,
+    ) -> Result<u8, zemu_sys::ViewError> {
+        let validator_items = self.validator.num_items() as u8;
+        let base_outputs_items = self.base_tx.base_outputs_num_items() as u8;
+        let stake_outputs_items = self.num_stake_items() as u8;
+
+        if item_n == 0 {
+            let label = pic_str!(b"AddDelegator");
+            title[..label.len()].copy_from_slice(label);
+            let content = pic_str!(b"Transaction");
+            return handle_ui_message(content, message, page);
+        }
+
+        let item_n = item_n - 1;
+
+        // when to start rendering staked outputs
+        let render_stake_outputs_at = validator_items + base_outputs_items;
+        let render_last_items_at = base_outputs_items + validator_items + stake_outputs_items;
+        let total_items = self.num_items() as u8;
+
+        match item_n {
+            // render base_outputs
+            x @ 0.. if x < base_outputs_items => self.render_base_outputs(x, title, message, page),
+
+            // render validator items
+            x if x >= base_outputs_items && x < render_stake_outputs_at => {
+                let new_idx = x - base_outputs_items;
+                self.validator.render_item(new_idx, title, message, page)
+            }
+
+            // render stake items
+            x if x >= render_stake_outputs_at
+                && x < (render_stake_outputs_at + stake_outputs_items) =>
+            {
+                let new_idx = x - render_stake_outputs_at;
+                self.render_stake_outputs(new_idx, title, message, page)
+            }
+
+            // render rewards to, delegate fee and fee
+            x if x >= render_last_items_at && x < total_items - 1 => {
+                // normalize index to zero
+                let new_idx = x - (base_outputs_items + stake_outputs_items + validator_items);
+                self.render_last_items(new_idx, title, message, page)
+            }
+            _ => Err(ViewError::NoData),
+        }
+    }
+}
+
+impl<'b> AddDelegatorTx<'b> {
+    fn fee(&'b self) -> Result<u64, ParserError> {
+        let sum_inputs = self.base_tx.sum_inputs_amount()?;
+
+        let base_outputs = self.base_tx.sum_outputs_amount()?;
+        let stake_outputs = Self::sum_stake_outputs_amount(&self.stake)?;
+
+        let total_outputs = base_outputs
+            .checked_add(stake_outputs)
+            .ok_or(ParserError::OperationOverflows)?;
+
+        let fee = sum_inputs
+            .checked_sub(total_outputs)
+            .ok_or(ParserError::OperationOverflows)?;
+        Ok(fee)
+    }
+
+    fn num_stake_items(&self) -> usize {
+        self.stake.iter().map(|output| output.num_items()).sum()
+    }
+
+    fn render_base_outputs(
+        &self,
+        item_n: u8,
+        title: &mut [u8],
+        message: &mut [u8],
+        page: u8,
+    ) -> Result<u8, ViewError> {
+        let (obj, item_idx) = self
+            .base_tx
+            .base_output_with_item(item_n)
+            .map_err(|_| ViewError::NoData)?;
+
+        // for base_outputs the header is Transfer
+        let header = pic_str!(b"Transfer");
+
+        self.render_output_with_header(&obj, item_idx, title, message, page, header)
+    }
+
+    fn render_stake_outputs(
+        &self,
+        item_n: u8,
+        title: &mut [u8],
+        message: &mut [u8],
+        page: u8,
+    ) -> Result<u8, ViewError> {
+        let (obj, item_idx) = self
+            .stake_output_with_item(item_n)
+            .map_err(|_| ViewError::NoData)?;
+
+        // for base_outputs the header is Transfer
+        let header = pic_str!(b"Stake");
+
+        self.render_output_with_header(&obj, item_idx, title, message, page, header)
+    }
+
+    // helper function to render any TransferableOutput<PvmOutput>,
+    // either locked or normal(comming as part of base_tx_fields)
+    // the rendering is the same, the only difference is that
+    // locked outputs uses a Stake label as the first item
+    fn render_output_with_header(
+        &'b self,
+        &obj: &TransferableOutput<'b, PvmOutput<'b>>,
+        item_n: u8,
+        title: &mut [u8],
+        message: &mut [u8],
+        page: u8,
+        header: &[u8],
+    ) -> Result<u8, ViewError> {
+        use lexical_core::Number;
+        //  'Transfer' or 'Stake':
+        //      '0.5 AVAX to
+        //  Address:
+        //      hrp + 1asxdpfsmah8wqr6m8ymfwse5e4pa9fwnvudmpn
+        //  Funds locked:
+        //      0.5 AVAX until 2021-05-31 21:28:00 UTC
+
+        // get the number of items for the obj wrapped up by PvmOutput
+        let num_inner_items = obj.output.num_inner_items() as _;
+
+        // do a custom rendering of the first base_output_items
+        match item_n {
+            0 => {
+                title[..header.len()].copy_from_slice(header);
+
+                let avax_to = PIC::new(AVAX_TO);
+                let mut content = [0; u64::FORMATTED_SIZE_DECIMAL + 2 + AVAX_TO.len()];
+
+                // write the amount
+                let amount = obj.amount().ok_or(ViewError::Unknown)?;
+                let len = nano_avax_to_fp_str(amount, &mut content[..])
+                    .map_err(|_| ViewError::Unknown)?
+                    .len();
+
+                // write avax
+                content[len..(len + avax_to.len())].copy_from_slice(avax_to.as_bytes());
+                handle_ui_message(&content[..], message, page)
+            }
+            // address rendering, according to avax team 99.99% of transactions only comes with one
+            // address, but we support rendering any
+            x @ 1.. if x < num_inner_items => {
+                // get the address index
+                let address_idx = x - 1;
+                let address = obj
+                    .output
+                    .get_address_at(address_idx as usize)
+                    .ok_or(ViewError::NoData)?;
+                // render encoded address with proper hrp,
+                let t = pic_str!(b"Address");
+                title[..t.len()].copy_from_slice(t);
+
+                let hrp = self.tx_header.hrp().map_err(|_| ViewError::Unknown)?;
+                let mut encoded = [0; MAX_ADDRESS_ENCODED_LEN];
+
+                let addr_len = address
+                    .encode_into(hrp, &mut encoded[..])
+                    .map_err(|_| ViewError::Unknown)?;
+
+                handle_ui_message(&encoded[..addr_len], message, page)
+            }
+            // by default we call the objects impl here,
+            // if it is a locked output, that info will be shown otherwise,
+            // this returns an error
+            _ => obj.render_item(item_n, title, message, page),
+        }
+    }
+
+    fn sum_stake_outputs_amount(
+        stake: &'b ObjectList<'b, TransferableOutput<PvmOutput<'b>>>,
+    ) -> Result<u64, ParserError> {
+        stake
+            .iter()
+            .filter_map(|output| output.amount())
+            .try_fold(0u64, |acc, x| acc.checked_add(x))
+            .ok_or(ParserError::OperationOverflows)
+    }
+
+    fn render_rewards_to(
+        &self,
+        addr_idx: usize,
+        title: &mut [u8],
+        message: &mut [u8],
+        page: u8,
+    ) -> Result<u8, zemu_sys::ViewError> {
+        let label = pic_str!(b"Rewards to");
+        title[..label.len()].copy_from_slice(label);
+
+        // render owner addresses
+        if let Some(addr) = self.rewards_owner.addresses.get(addr_idx) {
+            let hrp = self.tx_header.hrp().map_err(|_| ViewError::Unknown)?;
+
+            let mut address = MaybeUninit::uninit();
+            Address::from_bytes_into(addr, &mut address).map_err(|_| ViewError::Unknown)?;
+
+            let mut encoded = [0; MAX_ADDRESS_ENCODED_LEN];
+            // valid read as memory was initialized
+            let address = unsafe { address.assume_init() };
+
+            let len = address
+                .encode_into(hrp, &mut encoded[..])
+                .map_err(|_| ViewError::Unknown)?;
+
+            return handle_ui_message(&encoded[..len], message, page);
+        }
+
+        Err(ViewError::NoData)
+    }
+
+    fn render_last_items(
+        &self,
+        item_n: u8,
+        title: &mut [u8],
+        message: &mut [u8],
+        page: u8,
+    ) -> Result<u8, zemu_sys::ViewError> {
+        use lexical_core::{write as itoa, Number};
+
+        let mut buffer = [0; u64::FORMATTED_SIZE_DECIMAL + 2];
+        let num_addresses = self.rewards_owner.addresses.len() as u8;
+
+        match item_n {
+            // render rewards
+            x @ 0.. if x < num_addresses => {
+                self.render_rewards_to(x as usize, title, message, page)
+            }
+            x if x == num_addresses => {
+                let label = pic_str!(b"Fee");
+                title[..label.len()].copy_from_slice(label);
+
+                let fee = self.fee().map_err(|_| ViewError::Unknown)?;
+                let fee_buff =
+                    nano_avax_to_fp_str(fee, &mut buffer[..]).map_err(|_| ViewError::Unknown)?;
+                handle_ui_message(fee_buff, message, page)
+            }
+            _ => Err(ViewError::NoData),
+        }
+    }
+
+    // Gets the obj that contain the item_n, along with the index
+    // of the item. Returns an error otherwise
+    pub fn stake_output_with_item(
+        &'b self,
+        item_n: u8,
+    ) -> Result<(TransferableOutput<PvmOutput>, u8), ParserError> {
+        let mut count = 0usize;
+        let mut obj_item_n = 0;
+        // gets the output that contains item_n
+        // and its corresponding index
+        let filter = |o: &TransferableOutput<'b, PvmOutput>| -> bool {
+            let n = o.num_items();
+            for index in 0..n {
+                count += 1;
+                obj_item_n = index;
+                if count == item_n as usize + 1 {
+                    return true;
+                }
+            }
+            false
+        };
+
+        let obj = self
+            .stake
+            .iter()
+            .find(filter)
+            .ok_or(ParserError::DisplayIdxOutOfRange)?;
+        Ok((obj, obj_item_n as u8))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use lexical_core::Number;
+
+    const DATA: &[u8] = &[
+        // base tx:
+        0x00, 0x00, 0x00, 0x0e, 0x00, 0x00, 0x30, 0x39, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01,
+        //... localAssetId
+        0xdb, 0xcf, 0x89, 0x0f, 0x77, 0xf4, 0x9b, 0x96, 0x85, 0x76, 0x48, 0xb7, 0x2b, 0x77, 0xf9,
+        0xf8, 0x29, 0x37, 0xf2, 0x8a, 0x68, 0x70, 0x4a, 0xf0, 0x5d, 0xa0, 0xdc, 0x12, 0xba, 0x53,
+        0xf2, 0xdb, 0x00, 0x00, 0x00, 0x07, 0x00, 0x00, 0x00, 0x00, 0xee, 0x5b, 0xe5, 0xc0, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x01,
+        0xda, 0x2b, 0xee, 0x01, 0xbe, 0x82, 0xec, 0xc0, 0x0c, 0x34, 0xf3, 0x61, 0xed, 0xa8, 0xeb,
+        0x30, 0xfb, 0x5a, 0x71, 0x5c, 0x00, 0x00, 0x00, 0x01, 0xdf, 0xaf, 0xbd, 0xf5, 0xc8, 0x1f,
+        0x63, 0x5c, 0x92, 0x57, 0x82, 0x4f, 0xf2, 0x1c, 0x8e, 0x3e, 0x6f, 0x7b, 0x63, 0x2a, 0xc3,
+        0x06, 0xe1, 0x14, 0x46, 0xee, 0x54, 0x0d, 0x34, 0x71, 0x1a, 0x15, 0x00, 0x00, 0x00, 0x01,
+        // ... localAssetId,
+        0xdb, 0xcf, 0x89, 0x0f, 0x77, 0xf4, 0x9b, 0x96, 0x85, 0x76, 0x48, 0xb7, 0x2b, 0x77, 0xf9,
+        0xf8, 0x29, 0x37, 0xf2, 0x8a, 0x68, 0x70, 0x4a, 0xf0, 0x5d, 0xa0, 0xdc, 0x12, 0xba, 0x53,
+        0xf2, 0xdb, 0x00, 0x00, 0x00, 0x05,
+        // Have to override relative to the reference, as this
+        // doesn't provide enough funds for stake.
+        // 0x00, 0x00, 0x00, 0x00, 0xee, 0x6b, 0x28, 0x00,
+        0x00, 0x00, 0x01, 0xd2, 0x97, 0xb5, 0x48, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, // Node ID
+        0xe9, 0x09, 0x4f, 0x73, 0x69, 0x80, 0x02, 0xfd, 0x52, 0xc9, 0x08, 0x19, 0xb4, 0x57, 0xb9,
+        0xfb, 0xc8, 0x66, 0xab, 0x80, // StartTime
+        0x00, 0x00, 0x00, 0x00, 0x5f, 0x21, 0xf3, 0x1d, // EndTime
+        0x00, 0x00, 0x00, 0x00, 0x5f, 0x49, 0x7d, 0xc6, // Weight
+        0x00, 0x00, 0x01, 0xd1, 0xa9, 0x4a, 0x20, 0x00, // Stake
+        0x00, 0x00, 0x00, 0x01, //... localAssetId,
+        0xdb, 0xcf, 0x89, 0x0f, 0x77, 0xf4, 0x9b, 0x96, 0x85, 0x76, 0x48, 0xb7, 0x2b, 0x77, 0xf9,
+        0xf8, 0x29, 0x37, 0xf2, 0x8a, 0x68, 0x70, 0x4a, 0xf0, 0x5d, 0xa0, 0xdc, 0x12, 0xba, 0x53,
+        0xf2, 0xdb, 0x00, 0x00, 0x00, 0x07, 0x00, 0x00, 0x01, 0xd1, 0xa9, 0x4a, 0x20, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x01,
+        0x3c, 0xb7, 0xd3, 0x84, 0x2e, 0x8c, 0xee, 0x6a, 0x0e, 0xbd, 0x09, 0xf1, 0xfe, 0x88, 0x4f,
+        0x68, 0x61, 0xe1, 0xb2, 0x9c, // RewardsOwner
+        0x00, 0x00, 0x00, 0x0b, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x01, 0x00, 0x00, 0x00, 0x01, 0xda, 0x2b, 0xee, 0x01, 0xbe, 0x82, 0xec, 0xc0, 0x0c, 0x34,
+        0xf3, 0x61, 0xed, 0xa8, 0xeb, 0x30, 0xfb, 0x5a, 0x71, 0x5c,
+    ];
+
+    #[test]
+    fn parse_add_delegator() {
+        let (_, tx) = AddDelegatorTx::from_bytes(DATA).unwrap();
+        assert_eq!(tx.validator.weight, 2000000000000);
+    }
+}
