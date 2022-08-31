97f88cec3e0edc29ed93e6da8f9253740630591b
Natanael <neithanmo@gmail.com>
2022-08-30 18:23:04 -0600

    feat(paths) Add support for multiple signing and change_address paths

--- js/src/index.ts
+++ js/src/index.ts
@@ -15,7 +15,7 @@
  *  limitations under the License.
  ******************************************************************************* */
 import Transport from '@ledgerhq/hw-transport'
-import { serializePath, serializeHrp, serializeChainID, pathCoinType } from './helper'
+import { serializePath, serializePathSuffix, serializeHrp, serializeChainID, pathCoinType } from './helper'
 import { ResponseAddress, ResponseAppInfo, ResponseBase, ResponseSign, ResponseVersion, ResponseWalletId, ResponseXPub } from './types'
 import {
   CHUNK_SIZE,
@@ -29,6 +29,9 @@ import {
   P1_VALUES,
   PAYLOAD_TYPE,
   processErrorResponse,
+  FIRST_MESSAGE,
+  LAST_MESSAGE,
+  HASH_LEN,
 } from './common'
 
 export { LedgerError, Curve }
@@ -113,19 +116,43 @@ export default class AvalancheApp {
     return chunks
   }
 
-  private async signGetChunks(path: string, message: Buffer) {
-    return AvalancheApp.prepareChunks(message, serializePath(path))
+  private async signGetChunks(message: Buffer, path?: string) {
+    if (path === undefined) {
+      return AvalancheApp.prepareChunks(message, Buffer.alloc(0))
+    } else {
+      return AvalancheApp.prepareChunks(message, serializePath(path))
+    }
   }
 
-  private async signSendChunk(chunkIdx: number, chunkNum: number, chunk: Buffer, curve?: Curve, ins: number = INS.SIGN, evm = false): Promise<ResponseSign> {
+  private concatMessageAndChangePath(message: Buffer, path?: Array<string>): Buffer {
+      // data
+    let msg = Buffer.concat([message]);
+    // no change_path
+    if (path === undefined) {
+      let buffer = Buffer.alloc(1);
+      buffer.writeUInt8(0);
+      return Buffer.concat([buffer, msg]);
+    } else {
+      let buffer = Buffer.alloc(1);
+      buffer.writeUInt8(path.length);
+      path.forEach( (element) => {
+        buffer = Buffer.concat([buffer, serializePathSuffix(element)]);
+
+      });
+      return Buffer.concat([buffer, msg]);
+    }
+
+  }
+
+  private async signSendChunk(chunkIdx: number, chunkNum: number, chunk: Buffer, param?: Curve, ins: number = INS.SIGN, evm = false): Promise<ResponseSign> {
     let payloadType = PAYLOAD_TYPE.ADD
     let p2 = 0
     if (chunkIdx === 1) {
       payloadType = PAYLOAD_TYPE.INIT
-      if (curve === undefined) {
-        throw Error('curve type not given')
+      if (param === undefined) {
+        throw Error('curve or number type not given')
       }
-      p2 = curve
+      p2 = param
     }
     if (chunkIdx === chunkNum) {
       payloadType = PAYLOAD_TYPE.LAST
@@ -155,8 +182,8 @@ export default class AvalancheApp {
 
         if (returnCode === LedgerError.NoErrors && response.length > 2) {
           return {
-            hash: response.slice(0, 32),
-            signature: response.slice(32, -2),
+            hash: null,
+            signature: null,
             returnCode: returnCode,
             errorMessage: errorMessage,
           }
@@ -169,8 +196,129 @@ export default class AvalancheApp {
       }, processErrorResponse)
   }
 
-  async sign(path: string, message: Buffer): Promise<ResponseSign> {
-    const coinType = pathCoinType(path);
+  // The preamble consist of the root path which is set before sending
+  // the remaining data to be show/signed
+  async send_preamble(path: string, curve: Curve, evm = false): Promise<ResponseSign> {
+    const serializedPath = serializePath(path);
+    return this.transport
+      .send(CLA, INS.PREAMBLE, 0, curve, Buffer.concat([serializedPath]), [LedgerError.NoErrors])
+      .then((response: Buffer) => {
+        const errorCodeData = response.slice(-2)
+        const returnCode = errorCodeData[0] * 256 + errorCodeData[1]
+        let errorMessage = errorCodeToString(returnCode)
+
+        if (
+          returnCode === LedgerError.BadKeyHandle ||
+          returnCode === LedgerError.DataIsInvalid
+        ) {
+          errorMessage = `${errorMessage} : ${response.slice(0, response.length - 2).toString('ascii')}`
+        }
+        return {
+          returnCode: returnCode,
+          errorMessage: errorMessage,
+        }
+      }, processErrorResponse)
+  }
+
+   async signHash(path_prefix: string, signing_paths: Array<string>, hash: Buffer, is_eth = false): Promise<ResponseSign> {
+
+     if (hash.length !== HASH_LEN) {
+      throw new Error('Invalid hash length');
+     }
+
+    const cla = is_eth ? CLA_ETH : CLA;
+
+    // Set root path and curve
+    let response = await this.send_preamble(path_prefix, Curve.Secp256K1, is_eth);
+    if (response.returnCode !== LedgerError.NoErrors) {
+      return response;
+    }
+
+    // send hash
+    const first_response = await this.transport
+      .send(cla, INS.SIGN_HASH, FIRST_MESSAGE, 0x00, Buffer.concat([hash]), [LedgerError.NoErrors])
+      .then((response: Buffer) => {
+        const errorCodeData = response.slice(-2)
+        const returnCode = errorCodeData[0] * 256 + errorCodeData[1]
+        let errorMessage = errorCodeToString(returnCode)
+
+        if (
+          returnCode === LedgerError.BadKeyHandle ||
+          returnCode === LedgerError.DataIsInvalid
+        ) {
+          errorMessage = `${errorMessage} : ${response.slice(0, response.length - 2).toString('ascii')}`
+        }
+        return {
+          returnCode: returnCode,
+          errorMessage: errorMessage,
+        }
+      }, processErrorResponse)
+
+    if (first_response.returnCode !== LedgerError.NoErrors) {
+      return first_response;
+    }
+
+    return this._signAndCollect(signing_paths)
+  }
+
+  private async _signAndCollect(signing_paths: Array<string>): Promise<ResponseSign> {
+    // base response object to output on each iteration
+    let result = {
+      returnCode: LedgerError.NoErrors,
+      errorMessage:"",
+      hash: null,
+      signatures: null as null | Buffer | Map<string, Buffer>,
+    };
+
+    // where each pair path_suffix, signature are stored
+    let signatures = new Map();
+
+    for (let idx = 0; idx < signing_paths.length; idx++) {
+      const suffix = signing_paths[idx];
+      const path_buf = serializePathSuffix(suffix);
+
+      const p1 = idx >= signing_paths.length - 1 ? LAST_MESSAGE : 0x80;
+
+      // send path to sign hash that should be in device's ram memory
+      await this.transport.send(CLA, INS.SIGN_HASH, p1, 0x00, path_buf, [
+        LedgerError.NoErrors,
+        LedgerError.DataIsInvalid,
+        LedgerError.BadKeyHandle,
+        LedgerError.SignVerifyError,
+      ])
+      .then((response: Buffer) => {
+        const errorCodeData = response.slice(-2)
+        const returnCode = errorCodeData[0] * 256 + errorCodeData[1]
+        let errorMessage = errorCodeToString(returnCode)
+
+        if (
+          returnCode === LedgerError.BadKeyHandle ||
+          returnCode === LedgerError.DataIsInvalid ||
+          returnCode === LedgerError.SignVerifyError
+        ) {
+          result.errorMessage = `${errorMessage} : ${response.slice(0, response.length - 2).toString('ascii')}`
+        }
+
+        if (returnCode === LedgerError.NoErrors && response.length > 2) {
+          signatures.set(suffix, response.slice(0, -2));
+        }
+
+        result.returnCode = returnCode;
+        result.errorMessage = errorMessage;
+
+        return
+      }, processErrorResponse);
+
+      if (result.returnCode !== LedgerError.NoErrors) {
+        break;
+      }
+    };
+    result.signatures = signatures;
+    return result;
+  }
+
+  async sign(path_prefix: string, signing_paths: Array<string>, message: Buffer, change_paths?: Array<string>): Promise<ResponseSign> {
+    const coinType = pathCoinType(path_prefix);
 
     let is_eth: boolean;
     switch (coinType) {
@@ -183,14 +331,35 @@ export default class AvalancheApp {
       default:
         throw "Path's cointype should be either 60\' or 9000\'"
     }
+    let curve = Curve.Secp256K1;
 
-    return this.signGetChunks(path, message).then(chunks => {
-      return this.signSendChunk(1, chunks.length, chunks[0], Curve.Secp256K1, INS.SIGN, is_eth).then(async response => {
+    // Set the root path and the curve
+    let res_preamble = await this.send_preamble(path_prefix, curve, is_eth)
+    if (res_preamble.returnCode !== LedgerError.NoErrors) {
+      return res_preamble;
+    }
+
+    // Do not show outputs that go to the signers
+    let paths = signing_paths;
+    if (change_paths !== undefined ) {
+      paths = paths.concat(change_paths);
+    }
+
+    // Prepend change_paths to the message as the device do set which outputs should be
+    // shown at parsing
+    const msg = this.concatMessageAndChangePath(message, paths);
+
+    // Send transaction for review
+    let response = await this.signGetChunks(msg).then(chunks => {
+      return this.signSendChunk(1, chunks.length, chunks[0], curve, INS.SIGN, is_eth).then(async response => {
+        // initialize response
         let result = {
           returnCode: response.returnCode,
           errorMessage: response.errorMessage,
-          signature: null as null | Buffer,
+          signatures: null as null | Buffer | Map<string, Buffer>,
         }
+
+        // send chunks
         for (let i = 1; i < chunks.length; i += 1) {
           // eslint-disable-next-line no-await-in-loop
           result = await this.signSendChunk(1 + i, chunks.length, chunks[i], Curve.Secp256K1, INS.SIGN, is_eth)
@@ -201,6 +370,14 @@ export default class AvalancheApp {
         return result
       }, processErrorResponse)
     }, processErrorResponse)
+
+    if (response.returnCode !== LedgerError.NoErrors) {
+      return response;
+    }
+
+    // Transaction was approved so start iterating over signing_paths to sign
+    // and collect each signature
+    return this._signAndCollect(signing_paths);
   }
 
   async getVersion(): Promise<ResponseVersion> {
