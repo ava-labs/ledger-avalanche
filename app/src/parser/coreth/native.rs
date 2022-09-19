b32ffa8980eecb0796a113fbe98bf50774e100b4
Natanael <neithanmo@gmail.com>
2022-09-19 08:27:49 -0600

    feat: Ethereum transaction definition

--- app/src/parser/coreth/native.rs
+++ app/src/parser/coreth/native.rs
@@ -13,13 +13,17 @@
 *  See the License for the specific language governing permissions and
 *  limitations under the License.
 ********************************************************************************/
+use core::{mem::MaybeUninit, ptr::addr_of_mut};
 
 use nom::bytes::complete::take;
 use zemu_sys::ViewError;
 
 use crate::{
     handlers::{eth::u256, handle_ui_message},
-    parser::{intstr_to_fpstr_inplace, ParserError, U64_SIZE},
+    parser::{
+        intstr_to_fpstr_inplace, DisplayableItem, FromBytes, ParserError, EIP1559_TX, EIP2930_TX,
+        U64_SIZE,
+    },
 };
 
 mod legacy;
@@ -106,4 +110,146 @@ pub fn parse_rlp_item(data: &[u8]) -> Result<(&[u8], &[u8]), nom::Err<ParserErro
     };
 
     take(to_read as usize)(&data[read..])
+}
+
+// Important: do not change the repr attribute,
+// as this type is use as the tag field
+// for the Transaction enum which has the same representation
+#[derive(Clone, Copy, PartialEq)]
+#[cfg_attr(test, derive(Debug))]
+#[repr(u8)]
+pub enum EthTxType {
+    Legacy,
+    EIP1559,
+    EIP2930,
+}
+
+impl From<u8> for EthTxType {
+    fn from(value: u8) -> Self {
+        match value {
+            EIP1559_TX => Self::EIP1559,
+            EIP2930_TX => Self::EIP2930,
+            // legacy is constructed
+            _ => Self::Legacy,
+        }
+    }
+}
+
+impl EthTxType {
+    fn from_bytes(input: &[u8]) -> Result<(&[u8], Self), ParserError> {
+        if input.is_empty() {
+            return Err(ParserError::UnexpectedBufferEnd);
+        }
+
+        let tx_type = input.get(0).ok_or(ParserError::UnexpectedBufferEnd)?;
+
+        match *tx_type {
+            EIP1559_TX => Ok((&input[1..], Self::EIP1559)),
+            EIP2930_TX => Ok((&input[1..], Self::EIP2930)),
+            // legacy transaction does not have a version so just parse
+            // it, if it is not valid, the parser will error anyways
+            _ => Ok((input, Self::Legacy)),
+        }
+    }
+}
+
+#[repr(C)]
+struct LegacyVariant<'b>(EthTxType, Legacy<'b>);
+
+#[repr(C)]
+struct Eip1559Variant<'b>(EthTxType, Eip1559<'b>);
+
+#[repr(C)]
+struct Eip2930Variant<'b>(EthTxType, Eip2930<'b>);
+
+#[derive(Clone, Copy, PartialEq)]
+// DO not change the representation
+// as it would cause unalignment issues
+// with the OutputType tag
+#[repr(u8)]
+#[cfg_attr(test, derive(Debug))]
+pub enum EthTransaction<'b> {
+    Legacy(Legacy<'b>),
+    Eip1559(Eip1559<'b>),
+    Eip2930(Eip2930<'b>),
+}
+
+impl<'b> FromBytes<'b> for EthTransaction<'b> {
+    fn from_bytes_into(
+        input: &'b [u8],
+        out: &mut core::mem::MaybeUninit<Self>,
+    ) -> Result<&'b [u8], nom::Err<ParserError>> {
+        // get transaction data as the eip2718 defines transactions structure as follow:
+        // version || rlp[tx_fields]
+        // version for eip1559 = 2,
+        // for eip2930 = 1,
+        // for legacy it does not have a version
+        let (rem, tx_type) = EthTxType::from_bytes(input)?;
+
+        // parse rlp[] part in order to get the transaction bytes
+        let (rem, tx_bytes) = parse_rlp_item(rem)?;
+        if tx_bytes.is_empty() {
+            return Err(ParserError::UnexpectedBufferEnd.into());
+        }
+        match tx_type {
+            EthTxType::Legacy => {
+                let out = out.as_mut_ptr() as *mut LegacyVariant;
+
+                let legacy = unsafe { &mut *addr_of_mut!((*out).1).cast() };
+
+                // read all the data as the contract deployment
+                // we do not have a way to verify this data. in the worst scenario
+                // the transaction would be rejected, and for this reason
+                // It is shown on the screen(partially) for the user to review.
+                _ = Legacy::from_bytes_into(tx_bytes, legacy)?;
+            }
+            EthTxType::EIP1559 => {
+                let out = out.as_mut_ptr() as *mut Eip1559Variant;
+
+                let eip = unsafe { &mut *addr_of_mut!((*out).1).cast() };
+
+                // read all the data as the contract deployment
+                // we do not have a way to verify this data. in the worst scenario
+                // the transaction would be rejected, and for this reason
+                // It is shown on the screen(partially) for the user to review.
+                _ = Eip1559::from_bytes_into(tx_bytes, eip)?;
+            }
+            EthTxType::EIP2930 => {
+                let out = out.as_mut_ptr() as *mut Eip2930Variant;
+
+                let eip = unsafe { &mut *addr_of_mut!((*out).1).cast() };
+
+                // read all the data as the contract deployment
+                // we do not have a way to verify this data. in the worst scenario
+                // the transaction would be rejected, and for this reason
+                // It is shown on the screen(partially) for the user to review.
+                _ = Eip2930::from_bytes_into(tx_bytes, eip)?;
+            }
+        }
+        Ok(rem)
+    }
+}
+
+impl<'b> DisplayableItem for EthTransaction<'b> {
+    fn num_items(&self) -> usize {
+        match self {
+            Self::Legacy(t) => t.num_items(),
+            Self::Eip1559(t) => t.num_items(),
+            Self::Eip2930(t) => t.num_items(),
+        }
+    }
+
+    fn render_item(
+        &self,
+        item_n: u8,
+        title: &mut [u8],
+        message: &mut [u8],
+        page: u8,
+    ) -> Result<u8, ViewError> {
+        match self {
+            Self::Legacy(t) => t.render_item(item_n, title, message, page),
+            Self::Eip1559(t) => t.render_item(item_n, title, message, page),
+            Self::Eip2930(t) => t.render_item(item_n, title, message, page),
+        }
+    }
 }
