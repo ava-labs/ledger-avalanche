d709d600e6d03bf59b9d1a2f6de4759d4a8ad8d4
Natanael <neithanmo@gmail.com>
2022-09-21 15:24:46 -0600

    feat: Add basic tests and signature verification

--- zemu/tests/eth.test.ts
+++ zemu/tests/eth.test.ts
@@ -22,17 +22,37 @@ import { ec } from 'elliptic'
 const SIGN_TEST_DATA = [
   {
     name: 'transfer',
-    //ethereum tx hash: 0x7935135a927b1746b54e73c2b353daee96ba6aad6dd45683b36075b8092608fe
-    // this has been trimmed to remove the signature
     op: Buffer.from(
       '02f5018402a8af41843b9aca00850d8c7b50e68303d090944a2962ac08962819a8a17661970e3c0db765565e8817addd0864728ae780c0',
       'hex',
     ),
   },
+  {
+   
+    name: 'asset_transfer',
+    op: Buffer.from(
+      'f87c01856d6e2edc00830186a094010000000000000000000000000000000000000280b85441c9cc6fd27e26e70f951869fb09da685a696f0a79d338394f709c6d776d1318765981e69c09f0aa49864d8cc35699545b5e73a00000000000000000000000000000000000000000000000000123456789abcdef82a8688080',
+      'hex',
+    ),
+  },
+  {
+    name: 'asset_deposit',
+    op: Buffer.from(
+      'f87c01856d6e2edc00830186a094010000000000000000000000000000000000000280b85441c9cc6fd27e26e70f951869fb09da685a696f0a79d338394f709c6d776d1318765981e69c09f0aa49864d8cc35699545b5e73a00000000000000000000000000000000000000000000000000123456789abcdef82a8688080',
+      'hex',
+    ),
+  },
+  {
+    name: 'legacy_transfer',
+    op: Buffer.from(
+      'ed01856d6e2edc008252089428ee52a8f3d6e5d15f8b131996950d7f296c7952872bd72a248740008082a86a8080',
+      'hex',
+    ),
+  },
 ]
 
-describe.each(models)('Ethereum [%s]; sign', function (m) {
-  test.each(SIGN_TEST_DATA)('sign operation', async function (data) {
+describe.each(models)('EthereumTx [%s]; sign', function (m) {
+  test.each(SIGN_TEST_DATA)('sign transaction:  $name', async function (data) {
     const sim = new Zemu(m.path)
     try {
       await sim.start({ ...defaultOptions, model: m.name })
@@ -41,10 +61,9 @@ describe.each(models)('Ethereum [%s]; sign', function (m) {
 
       const testcase = `${m.prefix.toLowerCase()}-eth-sign-${data.name}`
 
-      await enableBlindSigning(sim, testcase)
       const currentScreen = sim.snapshot()
-      const respReq = app.signTransaction(ETH_DERIVATION, msg.toString('hex'))
 
+      const respReq = app.signTransaction(ETH_DERIVATION, msg.toString('hex'), null)
       await sim.waitUntilScreenIsNot(currentScreen, 20000)
       await sim.compareSnapshotsAndApprove('.', testcase)
 
@@ -57,7 +76,7 @@ describe.each(models)('Ethereum [%s]; sign', function (m) {
       expect(resp).toHaveProperty('v')
 
       //Verify signature
-      const resp_addr = await app.getAddress(ETH_DERIVATION, false)
+     const resp_addr = await app.getAddress(ETH_DERIVATION, false)
 
       const EC = new ec("secp256k1");
       const sha3 = require('js-sha3');
@@ -77,7 +96,7 @@ describe.each(models)('Ethereum [%s]; sign', function (m) {
   })
 })
 
-describe.each(models)('Ethereum [%s] - pubkey', function (m) {
+describe.each(models)('EthereumKeys [%s] - pubkey', function (m) {
   test('get pubkey and addr %s', async function () {
     const sim = new Zemu(m.path)
     try {
