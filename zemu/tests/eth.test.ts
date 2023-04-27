61267a79278a3516b304b5ccf3c1f298b37aa950
neithanmo <neithanmo@gmail.com>
2023-04-27 10:02:28 -0600

    fix(ci): Increase wait times to avoid timeout errors in CI

--- zemu/tests/eth.test.ts
+++ zemu/tests/eth.test.ts
@@ -123,7 +123,7 @@ describe.each(models)('EthereumTx [%s]; sign', function (m) {
 
 
       const respReq = app.signEVMTransaction(ETH_DERIVATION, msg.toString('hex'))
-      await sim.waitUntilScreenIsNot(currentScreen)
+      await sim.waitUntilScreenIsNot(currentScreen, 90000)
       await sim.compareSnapshotsAndApprove('.', testcase)
 
       const resp = await respReq
@@ -189,7 +189,7 @@ describe.each(models)('Ethereum [%s] - misc', function (m) {
 
 
       const respReq = app.signPersonalMessage(ETH_DERIVATION, msgData.toString('hex'))
-      await sim.waitUntilScreenIsNot(currentScreen)
+      await sim.waitUntilScreenIsNot(currentScreen, 20000)
       await sim.compareSnapshotsAndApprove('.', testcase)
 
       const resp = await respReq
