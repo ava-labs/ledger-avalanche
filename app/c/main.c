1de102b6db76f9187eca15a31ae96fcca92887be
neithanmo <neithanmo@gmail.com>
2023-04-28 18:08:53 -0600

    zeroize btc dispatcher state

--- app/c/main.c
+++ app/c/main.c
@@ -124,6 +124,7 @@ int main(void) {
     zemu_log_stack("main");
     #if !defined(TARGET_NANOS)
         initialize_app_globals();
+        btc_state_reset();
     #endif /* ifndef TARGET_NANOS */
 
     for (;;) {
