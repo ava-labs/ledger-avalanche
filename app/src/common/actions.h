ebfa12ae498575a3ab57acdbe32d0fcc0e2b7c24
neithanmo <neithanmo@gmail.com>
2024-05-14 08:39:19 -0600

    Remove custom eth implementations

--- app/src/common/actions.h
+++ app/src/common/actions.h
@@ -120,47 +120,11 @@ __Z_INLINE void app_sign_tx() {
     app_sign(offset);
 }
 
-__Z_INLINE void app_sign_eth() {
-    const uint8_t *message = tx_get_buffer();
-    const uint16_t messageLength = tx_get_buffer_length();
-    uint16_t replyLen = 0;
-
-    // MEMZERO(G_io_apdu_buffer, IO_APDU_BUFFER_SIZE);
-    // zxerr_t err = crypto_sign_eth(G_io_apdu_buffer, IO_APDU_BUFFER_SIZE - 3, message, messageLength, &replyLen);
-    zxerr_t err = crypto_sign_eth(G_io_apdu_buffer, IO_APDU_BUFFER_SIZE - 2, message, messageLength, &replyLen);
-
-    if (err != zxerr_ok || replyLen == 0) {
-        set_code(G_io_apdu_buffer, 0, APDU_CODE_SIGN_VERIFY_ERROR);
-        io_exchange(CHANNEL_APDU | IO_RETURN_AFTER_TX, 2);
-    } else {
-        set_code(G_io_apdu_buffer, replyLen, APDU_CODE_OK);
-        io_exchange(CHANNEL_APDU | IO_RETURN_AFTER_TX, replyLen + 2);
-    }
-}
-
 __Z_INLINE void app_sign_msg() {
     zemu_log_stack("app_sign");
 
     // no change paths list at the begining
     app_sign(0);
-}
-
-__Z_INLINE void app_sign_eth_msg() {
-    const uint8_t *message = tx_get_buffer();
-    const uint16_t messageLength = tx_get_buffer_length();
-    uint16_t replyLen = 0;
-
-    // MEMZERO(G_io_apdu_buffer, IO_APDU_BUFFER_SIZE);
-    // zxerr_t err = crypto_sign_eth(G_io_apdu_buffer, IO_APDU_BUFFER_SIZE - 3, message, messageLength, &replyLen);
-    zxerr_t err = crypto_sign_eth_msg(G_io_apdu_buffer, IO_APDU_BUFFER_SIZE - 2, message, messageLength, &replyLen);
-
-    if (err != zxerr_ok || replyLen == 0) {
-        set_code(G_io_apdu_buffer, 0, APDU_CODE_SIGN_VERIFY_ERROR);
-        io_exchange(CHANNEL_APDU | IO_RETURN_AFTER_TX, 2);
-    } else {
-        set_code(G_io_apdu_buffer, replyLen, APDU_CODE_OK);
-        io_exchange(CHANNEL_APDU | IO_RETURN_AFTER_TX, replyLen + 2);
-    }
 }
 
 __Z_INLINE void app_reject() {
@@ -178,19 +142,3 @@ __Z_INLINE void app_reply_error() {
     set_code(G_io_apdu_buffer, 0, APDU_CODE_DATA_INVALID);
     io_exchange(CHANNEL_APDU | IO_RETURN_AFTER_TX, 2);
 }
-
-__Z_INLINE void app_eth_configuration() {
-    MEMZERO(G_io_apdu_buffer, IO_APDU_BUFFER_SIZE);
-
-    uint16_t replyLen = 4;
-
-    G_io_apdu_buffer[0] = 0x02; //needs external data for ERC20 tokens
-    G_io_apdu_buffer[1] = APPVERSION_M;
-    G_io_apdu_buffer[2] = APPVERSION_N;
-    G_io_apdu_buffer[3] = APPVERSION_P;
-
-
-    set_code(G_io_apdu_buffer, replyLen, APDU_CODE_OK);
-    io_exchange(CHANNEL_APDU | IO_RETURN_AFTER_TX, replyLen + 2);
-}
-
