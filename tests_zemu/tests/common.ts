fd82a6b093c76637dee8873102b8ef2d90a6e68b
neithanmo <neithanmo@gmail.com>
2024-05-21 08:14:51 -0600

    try simplfying workflows minor fixes add rust tests check fix zemu tests

--- tests_zemu/tests/common.ts
+++ tests_zemu/tests/common.ts
@@ -4,6 +4,7 @@ const Resolve = require('path').resolve
 
 export const APP_SEED = 'equip will roof matter pink blind book anxiety banner elbow sun young'
 
+// Default to 'app' if app is not set
 const APP_PATH_S = Resolve('../app/output/app_s.elf')
 const APP_PATH_X = Resolve('../app/output/app_x.elf')
 const APP_PATH_SP = Resolve('../app/output/app_s2.elf')
