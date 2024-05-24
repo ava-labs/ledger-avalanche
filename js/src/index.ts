513e27c39cd8fe3f060860888c751839a114597c
neithanmo <neithanmo@gmail.com>
2024-05-24 12:10:10 -0600

    Add setPlugin and fix Nft provider

--- js/src/index.ts
+++ js/src/index.ts
@@ -21,6 +21,12 @@ import {
   CLA,
   CLA_ETH,
   COLLECTION_NAME_MAX_LEN,
+  ADDRESS_LENGTH,
+  ALGORITHM_ID_1,
+  ALGORITHM_ID_SIZE,
+  TYPE_SIZE,
+  VERSION_SIZE,
+  SIGNATURE_LENGTH_SIZE,
   CONTRACT_ADDRESS_LEN,
   errorCodeToString,
   FIRST_MESSAGE,
@@ -590,8 +596,148 @@ export default class AvalancheApp {
     return this.eth.provideERC20TokenInformation(buffer.toString('hex'))
   }
 
-  async provideNFTInformation(data: string): Promise<boolean> {
-    return this.eth.provideNFTInformation(data)
+  async provideNFTInformation(collectionName: string, contractAddress: string, chainId: bigint): Promise<boolean> {
+    const NAME_LENGTH_SIZE = 1
+    const HEADER_SIZE = TYPE_SIZE + VERSION_SIZE + NAME_LENGTH_SIZE
+    const KEY_ID_SIZE = 1
+    const PROD_NFT_METADATA_KEY = 1
+
+    const collectionNameLength = Buffer.byteLength(collectionName, 'utf8')
+
+    if (collectionNameLength > COLLECTION_NAME_MAX_LEN) {
+      throw new Error(`Collection name exceeds maximum allowed length of ${COLLECTION_NAME_MAX_LEN}`)
+    }
+
+    // We generate a fake signature, because verification is disabled
+    // in the app.
+    const fakeDerSignature = this._generateFakeDerSignature()
+
+    const buffer = Buffer.alloc(
+      HEADER_SIZE +
+        collectionNameLength +
+        ADDRESS_LENGTH +
+        CHAIN_ID_SIZE +
+        KEY_ID_SIZE +
+        ALGORITHM_ID_SIZE +
+        SIGNATURE_LENGTH_SIZE +
+        fakeDerSignature.length,
+    )
+
+    let offset = 0
+
+    buffer.writeUInt8(TYPE_1, offset)
+    offset += TYPE_SIZE
+
+    buffer.writeUInt8(VERSION_1, offset)
+    offset += VERSION_SIZE
+
+    buffer.writeUInt8(collectionNameLength, offset)
+    offset += NAME_LENGTH_SIZE
+
+    buffer.write(collectionName, offset, 'utf8')
+    offset += collectionNameLength
+
+    Buffer.from(contractAddress.slice(2), 'hex').copy(buffer, offset) // Remove '0x' from address
+    offset += ADDRESS_LENGTH
+
+    buffer.writeBigUInt64BE(chainId, offset)
+    offset += CHAIN_ID_SIZE
+
+    buffer.writeUInt8(PROD_NFT_METADATA_KEY, offset) // Assume production key for simplicity
+    offset += KEY_ID_SIZE
+
+    buffer.writeUInt8(ALGORITHM_ID_1, offset) // Assume a specific algorithm for signature or hash
+    offset += ALGORITHM_ID_SIZE
+
+    buffer.writeUInt8(fakeDerSignature.length, offset)
+    offset += SIGNATURE_LENGTH_SIZE
+
+    fakeDerSignature.copy(buffer, offset)
+
+    return this.eth.provideNFTInformation(buffer.toString('hex'))
+  }
+
+  _generateFakeDerSignature(): Buffer {
+    const fakeSignatureLength = 70
+    const fakeDerSignature = Buffer.alloc(fakeSignatureLength)
+
+    // Fill the buffer with random bytes
+    for (let i = 0; i < fakeSignatureLength; i++) {
+      fakeDerSignature[i] = Math.floor(Math.random() * 256)
+    }
+
+    return fakeDerSignature
+  }
+
+  // We assume pluginName is ERC721 for Nft tokens
+  async setPlugin(contractAddress: string, methodSelector: string, chainId: bigint): Promise<boolean> {
+    const KEY_ID = 2
+    const PLUGIN_NAME_LENGTH_SIZE = 1
+    const KEY_ID_SIZE = 1
+    const PLUGIN_NAME = 'ERC721'
+
+    const pluginNameBuffer = Buffer.from(PLUGIN_NAME, 'utf8')
+    const pluginNameLength = pluginNameBuffer.length
+
+    const contractAddressBuffer = Buffer.from(contractAddress.slice(2), 'hex')
+    const methodSelectorBuffer = Buffer.from(methodSelector.slice(2), 'hex')
+
+    // We generate a fake signature, because verification is disabled
+    // in the app.
+    const signatureBuffer = this._generateFakeDerSignature()
+    const signatureLength = signatureBuffer.length
+
+    const buffer = Buffer.alloc(
+      TYPE_SIZE +
+        VERSION_SIZE +
+        PLUGIN_NAME_LENGTH_SIZE +
+        pluginNameLength +
+        contractAddressBuffer.length +
+        methodSelectorBuffer.length +
+        CHAIN_ID_SIZE +
+        KEY_ID_SIZE +
+        ALGORITHM_ID_SIZE +
+        SIGNATURE_LENGTH_SIZE +
+        signatureLength,
+    )
+
+    let offset = 0
+
+    buffer.writeUInt8(TYPE_1, offset)
+    offset += TYPE_SIZE
+
+    buffer.writeUInt8(VERSION_1, offset)
+    offset += VERSION_SIZE
+
+    buffer.writeUInt8(pluginNameLength, offset)
+    offset += PLUGIN_NAME_LENGTH_SIZE
+
+    pluginNameBuffer.copy(buffer, offset)
+    offset += pluginNameLength
+
+    contractAddressBuffer.copy(buffer, offset)
+    offset += contractAddressBuffer.length
+
+    methodSelectorBuffer.copy(buffer, offset)
+    offset += methodSelectorBuffer.length
+
+    buffer.writeBigUInt64BE(BigInt(chainId), offset)
+    offset += CHAIN_ID_SIZE
+
+    // use default key_id
+    buffer.writeUInt8(KEY_ID, offset)
+    offset += KEY_ID_SIZE
+
+    // use default algorithm
+    buffer.writeUInt8(ALGORITHM_ID_1, offset)
+    offset += ALGORITHM_ID_SIZE
+
+    buffer.writeUInt8(signatureLength, offset)
+    offset += SIGNATURE_LENGTH_SIZE
+
+    signatureBuffer.copy(buffer, offset)
+
+    return this.eth.setPlugin(buffer.toString('hex'))
   }
 
   async clearSignTransaction(
