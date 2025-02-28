880a6dd53f43b73b668d44f60c6ff4ea402564d0
Carlos Medeiros <carlos.medeiros@zondax.ch>
2025-02-28 17:47:10 +0000

    add test

--- tests_zemu/tests/addr.test.ts
+++ tests_zemu/tests/addr.test.ts
@@ -24,6 +24,8 @@ const defaultOptions = (model: any) => {
 }
 
 const EXPECTED_PUBLIC_KEY = '02c6f477ff8e7136de982f898f6bfe93136bbe8dada6c17d0cd369acce90036ac4'
+const EXPECTED_ED25519_PUBLIC_KEY = 'f1d21c2c57e3beee8e74731f0792660948296112298e9801878b64f7df56cdb3'
+const EXPECTED_ED25519_ADDRESS = '00d0c7ea17d202255f53c1bb489489cb4e6c91988f5bdfb6f0a82fbebe4047ec557996eae8'
 
 jest.setTimeout(200000)
 
@@ -101,6 +103,49 @@ describe.each(models)('StandardPubKey [%s] - pubkey', function (m) {
 
       await sim.waitUntilScreenIsNot(sim.getMainMenuSnapshot())
       await sim.compareSnapshotsAndApprove('.', `${m.prefix.toLowerCase()}-zemu-addr`)
+
+      const resp = await respReq
+      console.log(resp, m.name)
+
+      expect(resp.returnCode).toEqual(0x9000)
+      expect(resp.errorMessage).toEqual('No errors')
+      expect(resp).toHaveProperty('publicKey')
+      expect(resp).toHaveProperty('hash')
+      expect(resp).toHaveProperty('address')
+    } finally {
+      await sim.close()
+    }
+  })
+ test('getPubkeyAddr-ed25519', async function () {
+    const sim = new Zemu(m.path)
+    try {
+      await sim.start(defaultOptions(m))
+      const app = new AvalancheApp(sim.getTransport())
+      const resp = await app.getAddressAndPubKey(APP_DERIVATION, false, undefined, undefined, 1)
+
+      console.log(resp, m.name)
+      console.log(Buffer.from(resp.address).toString('hex'))
+      expect(resp.returnCode).toEqual(0x9000)
+      expect(resp.errorMessage).toEqual('No errors')
+      expect(resp).toHaveProperty('publicKey')
+      expect(resp).toHaveProperty('hash')
+      expect(resp).toHaveProperty('address')
+      expect(resp.publicKey.toString('hex')).toEqual(EXPECTED_ED25519_PUBLIC_KEY)
+      expect(resp.address.toString('hex')).toEqual(EXPECTED_ED25519_ADDRESS)
+    } finally {
+      await sim.close()
+    }
+  })
+
+  test('ShowAddr-ed25519', async function () {
+    const sim = new Zemu(m.path)
+    try {
+      await sim.start(defaultOptions(m))
+      const app = new AvalancheApp(sim.getTransport())
+      const respReq = app.getAddressAndPubKey(APP_DERIVATION, true, undefined, undefined, 1)
+
+      await sim.waitUntilScreenIsNot(sim.getMainMenuSnapshot())
+      await sim.compareSnapshotsAndApprove('.', `${m.prefix.toLowerCase()}-addr-ed25519`)
 
       const resp = await respReq
       console.log(resp, m.name)
