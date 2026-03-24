d7d3888dfae6241325e9350fe81d71cb6647b658
Carlos Medeiros <carlos.medeiros@zondax.ch>
2026-03-24 12:20:47 +0000

    Add support for AddAutoRenewedValidator transactions (ACP-236)
    Implements the parser and UI rendering logic for the new auto-renewed validator transaction type. This change supports the continuous staking feature as defined in ACP-236.

--- app/rust/src/parser/transactions.rs
+++ app/rust/src/parser/transactions.rs
@@ -75,8 +75,10 @@ cfg_if! {
             RegisterL1ValidatorTx,
             SetL1ValidatorWeightTx,
             DisableL1ValidatorTx,
-            IncreaseL1ValidatorBalanceTx
+            IncreaseL1ValidatorBalanceTx,
 
+            // ACP-236
+            AddAutoRenewedValidatorTx
         };
     }
 }
@@ -110,7 +112,8 @@ cfg_if! {
     if #[cfg(feature = "banff")] {
         use super::{
             PVM_ADD_PERMISSIONLESS_DELEGATOR, PVM_ADD_PERMISSIONLESS_VALIDATOR, PVM_TRANSFORM_SUBNET, PVM_REMOVE_SUBNET_VALIDATOR,
-            PVM_CONVERT_SUBNET_L1, PVM_INCREASE_L1_VALIDATOR_BALANCE, PVM_DISABLE_L1_VALIDATOR, PVM_SET_L1_VALIDATOR_WEIGHT, PVM_REGISTER_L1_VALIDATOR
+            PVM_CONVERT_SUBNET_L1, PVM_INCREASE_L1_VALIDATOR_BALANCE, PVM_DISABLE_L1_VALIDATOR, PVM_SET_L1_VALIDATOR_WEIGHT, PVM_REGISTER_L1_VALIDATOR,
+            PVM_ADD_AUTO_RENEWED_VALIDATOR
         };
     }
 }
@@ -169,6 +172,8 @@ impl TryFrom<(u32, NetworkInfo)> for Transaction__Type {
             PVM_DISABLE_L1_VALIDATOR => Transaction__Type::DisableL1Validator,
             #[cfg(feature = "banff")]
             PVM_INCREASE_L1_VALIDATOR_BALANCE => Transaction__Type::IncreaseL1ValidatorBalance,
+            #[cfg(feature = "banff")]
+            PVM_ADD_AUTO_RENEWED_VALIDATOR => Transaction__Type::AutoRenewedValidator,
             _ => return Err(ParserError::InvalidTransactionType),
         };
 
@@ -219,6 +224,8 @@ pub enum Transaction<'b> {
     DisableL1Validator(DisableL1ValidatorTx<'b>),
     #[cfg(feature = "banff")]
     IncreaseL1ValidatorBalance(IncreaseL1ValidatorBalanceTx<'b>),
+    #[cfg(feature = "banff")]
+    AutoRenewedValidator(AddAutoRenewedValidatorTx<'b>),
 }
 
 impl<'b> Transaction<'b> {
@@ -269,6 +276,8 @@ impl<'b> Transaction<'b> {
             Self::PermissionlessValidator(tx) => tx.disable_output_if(address),
             #[cfg(feature = "banff")]
             Self::PermissionlessDelegator(tx) => tx.disable_output_if(address),
+            #[cfg(feature = "banff")]
+            Self::AutoRenewedValidator(tx) => tx.disable_output_if(address),
             _ => {}
         }
     }
@@ -381,6 +390,13 @@ impl<'b> Transaction<'b> {
                     out,
                 )
             }
+            #[cfg(feature = "banff")]
+            Transaction__Type::AutoRenewedValidator => {
+                Self::init_as_auto_renewed_validator(
+                    |out| AddAutoRenewedValidatorTx::from_bytes_into(input, out),
+                    out,
+                )
+            }
         }
     }
 
@@ -432,6 +448,8 @@ impl DisplayableItem for Transaction<'_> {
             Self::SetL1ValidatorWeight(tx) => tx.num_items(),
             #[cfg(feature = "banff")]
             Self::RegisterL1Validator(tx) => tx.num_items(),
+            #[cfg(feature = "banff")]
+            Self::AutoRenewedValidator(tx) => tx.num_items(),
         }
     }
 
@@ -482,6 +500,8 @@ impl DisplayableItem for Transaction<'_> {
             Self::SetL1ValidatorWeight(tx) => tx.render_item(item_n, title, message, page),
             #[cfg(feature = "banff")]
             Self::RegisterL1Validator(tx) => tx.render_item(item_n, title, message, page),
+            #[cfg(feature = "banff")]
+            Self::AutoRenewedValidator(tx) => tx.render_item(item_n, title, message, page),
         }
     }
 }
--- app/rust/src/parser/transactions/pvm/banff.rs
+++ app/rust/src/parser/transactions/pvm/banff.rs
@@ -39,3 +39,6 @@ pub use set_l1_validator_weight::*;
 
 mod register_l1_validator;
 pub use register_l1_validator::*;
+
+mod add_auto_renewed_validator;
+pub use add_auto_renewed_validator::*;
