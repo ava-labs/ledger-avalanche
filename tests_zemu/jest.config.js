d461acbdf303dee5ed7813b072fa24e2c1c13df7
Carlos Medeiros <carlos.medeiros@zondax.ch>
2026-05-18 16:39:40 +0100

    chore: Bump @zondax/zemu to 0.67.4

--- tests_zemu/jest.config.js
+++ tests_zemu/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  transformIgnorePatterns: ['node_modules/(?!(@ethereumjs|@noble|get-port)/)'],
+  transformIgnorePatterns: ['node_modules/(?!(\\.pnpm/)?(@ethereumjs|@noble|get-port)[+/@])'],
   transform: {
     '^.+\\.[jt]sx?$': 'ts-jest',
   },
